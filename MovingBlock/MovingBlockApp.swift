ff16287d42dd42e98b53c1f6cb34c3eddb668541
Yurii Tyshchenko <orty222@gmail.com>
2023-12-12 16:33:01 +0100

    Initial Commit

---
+++ MovingBlock/MovingBlockApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovingBlockApp.swift
+//  MovingBlock
+//
+//  Created by student on 12/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct MovingBlockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
